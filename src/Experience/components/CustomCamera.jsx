8b6fba8fe62df091b5263663eb2076a6e57f3070
Andrew Woan <andrewwoan@gmail.com>
2026-02-16 12:42:19 -0500

    SHEESH DA BBAIE

--- src/Experience/components/CustomCamera.jsx
+++ src/Experience/components/CustomCamera.jsx
@@ -3,27 +3,33 @@ import { useGSAP } from "@gsap/react";
 import { useFrame, useThree } from "@react-three/fiber";
 import React, { useMemo, useRef } from "react";
 import { useCurveProgressStore } from "../../store/useCurveProgressStore";
+import * as THREE from "three";
 
 const CustomCamera = () => {
   const { camera } = useThree();
   const curves = useCurveProgressStore((state) => state.curves);
+  const targetPosition = useRef(new THREE.Vector3(0, 0, 0));
+  const currentLookAt = useRef(new THREE.Vector3(0, 0, 0));
+  const targetLookAt = useRef(new THREE.Vector3(0, 0, 0));
 
   useFrame(() => {
     const scrollProgressPosition =
       useCurveProgressStore.getState().scrollProgress;
-
     const offsetScrollCameraPosition = (scrollProgressPosition + 0.5) % 1;
 
-    const cameraCurvePosition = curves.cameraPathCurve.getPointAt(
+    curves.cameraPathCurve.getPointAt(
       offsetScrollCameraPosition,
+      targetPosition.current,
     );
-
-    const cameraLookAtCurvePosition = curves.cameraLookAtCurve.getPointAt(
+    curves.cameraLookAtCurve.getPointAt(
       scrollProgressPosition,
+      targetLookAt.current,
     );
 
-    camera.position.copy(cameraCurvePosition);
-    camera.lookAt(cameraLookAtCurvePosition);
+    camera.position.lerp(targetPosition.current, 0.1);
+    currentLookAt.current.lerp(targetLookAt.current, 0.1);
+
+    camera.lookAt(currentLookAt.current);
   });
 
   return <></>;
